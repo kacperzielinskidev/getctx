160d6d5e001cd94dafb129c153478004f77242a3
kpdve <kacperzielinski.dev@gmail.com>
2025-09-29 16:23:04 +0200

    refactor: remove redundant logging statement during initialization

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -19,12 +19,10 @@ func Run() error {
 	defer cleanup()
 
 	log := logger.New(cfg.logOutput, cfg.logLevel)
-	log.Info("main", "Logger initialized successfully.")
-
 	fsys := fs.NewOSFileSystem()
 	appConfig := config.NewConfig()
 	contextBuilder := build.NewContextBuilder(log, fsys, cfg.outputFilename, appConfig)
-
 	app := core.NewApp(log, contextBuilder, appConfig, fsys, cfg.startPath)
+
 	return app.Run()
 }
